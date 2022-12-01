d2df6c7fa32510aa737b58551d01fa47cfd7c086
SohamE <soham.584sengupta@gmail.com>
2022-12-01 22:21:49 +0530

    Disadvantage of object using useState hook

--- src/index.js
+++ src/index.js
@@ -4,33 +4,71 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+// const Apple = (props) => {
+//   let [count, setCount] = useState(props.counter);
+//   let [text, setText] = useState("");
+
+//   const increment = () => {
+//     setCount(count+1);
+//   };
+
+//   return (
+//     <div>
+//       <input onChange={(e) => {setText(e.target.value)}}/>
+//       <p>The current {text || "count"} is {count}</p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={() => {setCount(props.counter)}}>reset</button>
+//       <button onClick={() => {setCount(count - 1)}}>-1</button>
+//     </div>
+//   );
+// };
 const Apple = (props) => {
-  let [count, setCount] = useState(props.counter);
-  let [text, setText] = useState("");
+  let [state, setState] = useState({
+    count: props.counter,
+    text: "",
+  });
 
   const increment = () => {
-    setCount(count+1);
+    setState({ ...state, count: state.count + 1 });
   };
 
   return (
     <div>
-      <input onChange={(e) => {setText(e.target.value)}}/>
-      <p>The current {text || "count"} is {count}</p>
+      <input
+        onChange={(e) => {
+          setState({ ...state, text: e.target.value });
+        }}
+      />
+      <p>
+        The current {state.text || "count"} is {state.count}
+      </p>
       <button onClick={increment}>+1</button>
-      <button onClick={() => {setCount(props.counter)}}>reset</button>
-      <button onClick={() => {setCount(count - 1)}}>-1</button>
+      <button
+        onClick={() => {
+          setState({ ...state, count: props.counter});
+        }}
+      >
+        reset
+      </button>
+      <button
+        onClick={() => {
+          setState({ ...state, count: state.count - 1});
+        }}
+      >
+        -1
+      </button>
     </div>
   );
 };
 
 Apple.defaultProps = {
-  counter: 0
-}
+  counter: 0,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Apple counter={30}/>
+    <Apple counter={30} />
   </React.StrictMode>
 );
 
